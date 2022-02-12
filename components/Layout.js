c902e9da5082cbbcd730e5e424d4218dcbdd3e44
lucasBmm <lucassbomfimm@gmail.com>
2022-02-12 14:29:39 -0300

    First Commit

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head';
+import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import useStyles from '../utils/styles';
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Head>
+        <title>Next E-commerce</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Typography>Amazona</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. E-Commerce.</Typography>
+      </footer>
+    </div>
+  );
+}
