1a140e12dce187c3c42e7f9a56108dad21a781a5
lucasBmm <lucassbomfimm@gmail.com>
2022-02-22 17:51:27 -0300

    shipping page

--- pages/shipping.js
+++ pages/shipping.js
@@ -12,20 +12,30 @@ import { Store } from '../utils/store';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import { Controller, useForm } from 'react-hook-form';
+import CheckoutWizard from '../components/checkoutWizard';
 
 export default function Shipping() {
   const {
     handleSubmit,
     control,
     formState: { errors },
+    setValue,
   } = useForm();
   const { state, dispatch } = useContext(Store);
   const router = useRouter();
-  const { userInfo } = state;
+  const {
+    userInfo,
+    cart: { shippingAddress },
+  } = state;
   useEffect(() => {
     if (!userInfo) {
       router.push('/login?redirect=/shipping');
     }
+    setValue('fullName', shippingAddress.fullName);
+    setValue('address', shippingAddress.address);
+    setValue('city', shippingAddress.city);
+    setValue('postalCode', shippingAddress.postalCode);
+    setValue('country', shippingAddress.country);
   }, []);
 
   const classes = useStyles();
@@ -35,21 +45,25 @@ export default function Shipping() {
       type: 'SAVE_SHIPPING_ADDRESS',
       payload: { fullName, address, city, postalCode, country },
     });
-    Cookies.set('shippingAddress', {
-      fullName,
-      address,
-      city,
-      postalCode,
-      country,
-    });
+    Cookies.set(
+      'shippingAddress',
+      JSON.stringify({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
+    );
     router.push('/payment');
   };
 
   return (
-    <Layout title="Shipping">
+    <Layout title="Shipping Address">
+      <CheckoutWizard activeStep={1} />
       <form className={classes.form} onSubmit={handleSubmit(submitHandler)}>
         <Typography component="h1" variant="h1">
-          Shipping
+          Shipping Address
         </Typography>
         <List>
           <ListItem>
@@ -69,10 +83,122 @@ export default function Shipping() {
                   label="Full name"
                   error={Boolean(errors.fullName)}
                   helperText={
-                    errors.name
-                      ? errors.name.type === 'minLength'
-                        ? 'Full name must be at least 2 caracter long'
+                    errors.fullName
+                      ? errors.fullName.type === 'minLength'
+                        ? 'Full name must be at least 5 caracter long'
                         : 'Full name is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="address"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 5,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="address"
+                  label="Address"
+                  error={Boolean(errors.address)}
+                  helperText={
+                    errors.address
+                      ? errors.address.type === 'minLength'
+                        ? 'Address must be at least 5 caracter long'
+                        : 'Address is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="city"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 5,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="city"
+                  label="City"
+                  error={Boolean(errors.city)}
+                  helperText={
+                    errors.city
+                      ? errors.city.type === 'minLength'
+                        ? 'City must be at least 5 caracter long'
+                        : 'City is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="postalCode"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 5,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="postalCode"
+                  label="Postal code"
+                  error={Boolean(errors.postalCode)}
+                  helperText={
+                    errors.postalCode
+                      ? errors.postalCode.type === 'minLength'
+                        ? 'Postal code must be at least 5 caracter long'
+                        : 'Postal code is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="country"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 5,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="country"
+                  label="Country"
+                  error={Boolean(errors.country)}
+                  helperText={
+                    errors.country
+                      ? errors.country.type === 'minLength'
+                        ? 'Country code must be at least 5 caracter long'
+                        : 'Country code is required'
                       : ''
                   }
                   {...field}
