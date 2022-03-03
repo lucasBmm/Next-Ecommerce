97311ae4f7ba250f13ce2768565071c5548632a9
LucasBmm <lucassbomfimm@gmail.com>
2022-03-03 15:11:16 -0300

    place order

--- utils/store.js
+++ utils/store.js
@@ -12,7 +12,7 @@ const initialState = {
       ? JSON.parse(Cookies.get('shippingAddress'))
       : { location: {} },
     paymentMethod: Cookies.get('paymentMethod')
-      ? Cookies.get('paymentMethod')
+      ? JSON.parse(Cookies.get('paymentMethod'))
       : '',
   },
   userInfo: Cookies.get('userInfo')
